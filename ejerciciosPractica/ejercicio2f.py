671f294499725e3fa179920c9f5026029daaf9a7
Jose <garcianavarro.jmiguel@gmail.com>
2023-12-01 12:47:11 +0100

    2023/12/01

--- ejerciciosPractica/ejercicio2f.py
+++ ejerciciosPractica/ejercicio2f.py
@@ -12,16 +12,20 @@ if numTiradas <= 1 or numTiradas > 5:
 else:
     listaTiradasJ1=[random.randint(1,6) for i in range(numTiradas)]
     print(listaTiradasJ1)
+
     maxResultadoJ1=max(listaTiradasJ1)
     minResultadoJ1=min(listaTiradasJ1)
     sumPuntosJ1=maxResultadoJ1+minResultadoJ1
+
     print(sumPuntosJ1)
 
     listaTiradasJ2=[random.randint(1,6) for i in range(numTiradas)]
     print(listaTiradasJ2)
+
     maxResultadoJ2=max(listaTiradasJ2)
     minResultadoJ2=min(listaTiradasJ2)
     sumPuntosJ2=maxResultadoJ2+minResultadoJ2
+
     print(sumPuntosJ2)
 
     if sumPuntosJ1 > sumPuntosJ2:
@@ -30,4 +34,3 @@ else:
         print("Jugador 2 gana!!")
     else:
         print("Empate!!")
-
