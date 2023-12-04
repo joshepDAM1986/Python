948109732be79be559e7cd98f684ae861f2f3da5
Jose <garcianavarro.jmiguel@gmail.com>
2023-12-04 12:17:40 +0100

    2023/12/01

--- examenPython/ejercicio2.py
+++ examenPython/ejercicio2.py
@@ -5,26 +5,60 @@ numTiradas = int(input("Número de tiradas: "))
 if numTiradas <0:
     print("Error")
 else:
-    JugadorA=[random.randint(1,6) for i in range(numTiradas)]
-    print(JugadorA)
-    maxResultadoJA=max(JugadorA)
-    minResultadoJA=min(JugadorA)
-    sumPuntosJA=maxResultadoJA+minResultadoJA
-    print(sumPuntosJA)
+    # JugadorA=[random.randint(1,6) for i in range(numTiradas)]
+    # print(JugadorA)
+    # maxResultadoJA=max(JugadorA)
+    # minResultadoJA=min(JugadorA)
+    # sumPuntosJA=maxResultadoJA+minResultadoJA
+    # print(sumPuntosJA)
+    #
+    # JugadorB=[random.randint(1,6) for i in range(numTiradas)]
+    # print(JugadorB)
+    # maxResultadoJB=max(JugadorB)
+    # minResultadoJB=min(JugadorB)
+    # sumPuntosJB=maxResultadoJB+minResultadoJB
+    # print(sumPuntosJB)
+    #
+    # if sumPuntosJA > sumPuntosJB:
+    #     print("Jugador 1 gana!!")
+    # elif sumPuntosJB > sumPuntosJA:
+    #     print("Jugador 2 gana!!")
+    # else:
+    #     print("Empate!!")
 
-    JugadorB=[random.randint(1,6) for i in range(numTiradas)]
-    print(JugadorB)
-    maxResultadoJB=max(JugadorB)
-    minResultadoJB=min(JugadorB)
-    sumPuntosJB=maxResultadoJB+minResultadoJB
-    print(sumPuntosJB)
+    valorMin=7
+    valorMax=0
+    valorTotalJA=0
+    valorTotalJB=0
 
-    if sumPuntosJA > sumPuntosJB:
-        print("Jugador 1 gana!!")
-    elif sumPuntosJB > sumPuntosJA:
-        print("Jugador 2 gana!!")
+    for i in range(numTiradas):
+        numObtenido=random.randint(1,6)
+        if numObtenido>valorMax:
+            valorMax=numObtenido
+        if numObtenido<valorMin:
+            valorMin=numObtenido
+        valorTotalJA=valorMin+valorMax
+    print(f"valor maximo JA: {valorMax}")
+    print(f"valor minimo JA: {valorMin}")
+    print(f"total JA: {valorTotalJA}")
+
+    for i in range(numTiradas):
+        numObtenido = random.randint(1, 6)
+        if numObtenido > valorMax:
+            valorMax = numObtenido
+        if numObtenido < valorMin:
+            valorMin = numObtenido
+        valorTotalJB = valorMin + valorMax
+    print(f"valor maximo JB: {valorMax}")
+    print(f"valor minimo JB: {valorMin}")
+    print(f"total JB: {valorTotalJB}")
+
+    if valorTotalJA>valorTotalJB:
+        print("Jugador A gana")
+    elif valorTotalJB>valorTotalJA:
+        print("Jugador B gana")
     else:
-        print("Empate!!")
+        print("Empate")
 
 
 longContraseñas=int(input("Ingrese la longitud de las contraseñas: "))
